d299ed9637d14cd88f8fb491a8af976e4e4d382d
carolina-92 <31933571+carolina-92@users.noreply.github.com>
2017-09-19 04:21:03 -0300

    Perimetro de un cuadrado
    Se pide la base de un cuadrado y se informa el perímetro

---
+++ Ejercicio9.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ext83235
+ */
+public class Ejercicio9 {
+
+    /**
+     * @param args the command line arguments
+     * Se pide la base de un cuadrado y se informa el perímetro 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int base ;
+        int perimetro;
+        
+        Scanner perimetroacalcular = new Scanner (System.in);
+        System.out.println("Ingrese base del cuadrado");
+        base = perimetroacalcular.nextInt();
+        perimetro = 4*base ;
+        System.out.println("El perimetro es " +perimetro);
+    }
+    
+}

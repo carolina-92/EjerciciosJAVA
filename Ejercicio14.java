d6f01310089226ce0a67ceba11406f93e156e66f
carolina-92 <31933571+carolina-92@users.noreply.github.com>
2017-09-19 04:28:12 -0300

    Producto mas 35%
    Se ingresa el importe de un producto importado y se le debe agregar el 35 % de impuestos
    internos.

---
+++ Ejercicio14.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication14;
+
+/**
+ *
+ * @author ext83235
+ */
+public class JavaApplication14 {
+
+    /**
+     * @param args the command line arguments
+     * Se ingresa el importe de un producto importado y se le debe agregar el 35 % de impuestos
+internos.
+
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int Importeproducto
+        int Iva
+
+        Scanner ivaproducto = New Scanner (System.in) ;
+        System.out.println("Ingrese importe del producto importado");
+        Importeproducto = ivaproducto.nextInt ();
+        iva= Importeproducto + 35/100
+    }
+    
+}
